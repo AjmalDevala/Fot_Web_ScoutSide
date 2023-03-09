f5853c3537ed1323de62067da279df89bdf631b3
AjmalDevala <ajmaldevala@gmail.com>
2023-03-09 11:34:22 +0530

    complete

--- src/page/SignupPage.jsx
+++ src/page/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignUp from '../components/auth/signup'
+import SignUp from '../components/auth/Signup'
 import Footer from '../components/layout/Footer'
 
 const SignupPage = () => {
